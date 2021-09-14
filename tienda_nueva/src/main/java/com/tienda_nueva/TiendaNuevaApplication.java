76ff5257c96ce8bbd89d132dcd376d746b2562fc
agarcia3009 <89949733+agarcia3009@users.noreply.github.com>
2021-09-13 22:56:05 -0500

    AGH

---
+++ tienda_nueva/src/main/java/com/tienda_nueva/TiendaNuevaApplication.java
@@ -0,0 +1,13 @@
+package com.tienda_nueva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaNuevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaNuevaApplication.class, args);
+	}
+
+}
